412a6526a29533a1787e5e24c39084e2d6a9f978
aclark <aclark@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2008-07-23 22:42:46 +0000

    Move trunk to egg.

---
+++ Products.OrderableReferenceField/trunk/Products/OrderableReferenceField/config.py
@@ -0,0 +1,15 @@
+PROJECTNAME = 'OrderableReferenceField'
+
+
+STYLESHEETS = (
+    {'id': 'orderablereference.css', 'media': 'screen', 'rendering': 'import'},
+        )
+
+JAVASCRIPTS = (
+    {'id': 'orderablereference.js',},
+        )
+
+# Two demo types are avaibable, these are used for testing purpose and are
+# disabled by default.
+
+REGISTER_DEMO_TYPES = False
